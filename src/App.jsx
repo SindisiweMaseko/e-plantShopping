7222372a3b2c2bae3f84e6cf6ed886a736e19365
SindisiweMaseko <sindisiweangelmaseko@gmail.com>
2026-05-21 21:24:32 +0200

    Complete project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,15 @@
+import React from 'react';
 
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
-
-function App() {
-  
-  const [showProductList, setShowProductList] = useState(false);
-
-  const handleGetStartedClick = () => {
-    setShowProductList(true);
-  };
-
-  const handleHomeClick = () => {
-    setShowProductList(false);
-  };
-
+function AboutUs() {
   return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
-
-      </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList onHomeClick={handleHomeClick}/>
-      </div>
+    <div className="about-us-container">
+      <p>
+        Welcome to Paradise Nursery, where green dreams flourish! Our mission is to transform everyday spaces into pristine natural sanctuaries by providing sustainably sourced, top-tier houseplants directly to your doorstep. Whether you are an expert green thumb or a beginner looking for low-maintenance companion plants, we have something beautiful for your space.
+      </p>
     </div>
   );
 }
 
-export default App;
-
+export default AboutUs;
 
 
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
+import cartReducer from './CartSlice'; // Points to CartSlice in the same folder
+
+const store = configureStore({
     reducer: {
         cart: cartReducer,
     },
 });
-export default store
+
+export default store;
